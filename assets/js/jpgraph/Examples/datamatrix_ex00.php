a4ad21d0702e4f79710b6f983e6b045a96d6051f
mambax7 <mambax7@gmail.com>
2022-01-02 04:59:24 -0500

    jpgraph via composer

--- assets/js/jpgraph/Examples/datamatrix_ex00.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . '/jpgraph/datamatrix/datamatrix.inc.php';
-
-$data = 'The first datamatrix';
-
-$encoder = DatamatrixFactory::Create();
-$backend = DatamatrixBackendFactory::Create($encoder);
-$backend->Stroke($data);
